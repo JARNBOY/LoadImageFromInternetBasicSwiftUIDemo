d6762c8f899d1ab09573789b1592d1f2b60f2d0a
Papon <papon.s@lawyerplusgroup.com>
2023-06-19 09:46:40 +0700

    Initial Commit

---
+++ LoadImageFromInternetBasicDemo/LoadImageFromInternetBasicDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadImageFromInternetBasicDemoApp.swift
+//  LoadImageFromInternetBasicDemo
+//
+//  Created by Papon Supamongkonchai on 19/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct LoadImageFromInternetBasicDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
